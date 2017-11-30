d9c2fd24358efac86dcf4b6b93c6bdcf129f721d
David Moles <david.moles@ucop.edu>
2017-11-30 14:59:59 -0800

    Implement skeleton enum generation

--- cursive-inv-codegen/src/main/java/org/cdlib/cursive/Generators.java
+++ cursive-inv-codegen/src/main/java/org/cdlib/cursive/Generators.java
@@ -1,24 +1,68 @@
 package org.cdlib.cursive;
 
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
-import io.vavr.collection.Array;
-import io.vavr.collection.LinkedHashMap;
-import org.jruby.RubyArray;
-import org.jruby.RubyClass;
+import io.vavr.collection.*;
+import org.apache.commons.lang3.StringUtils;
 import org.jruby.RubyHash;
 import org.jruby.embed.ScriptingContainer;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
 
 @SuppressWarnings("unchecked")
 class Generators {
 
+  public static final String CURSIVE_RTF_PACKAGE = "org.cdlib.cursive.rtf";
+  private static final ClassName VOCAB_CLASS_NAME = ClassName.get(CURSIVE_RTF_PACKAGE, "Vocabulary");
+  private static final ClassName TERM_CLASS_NAME = ClassName.get(CURSIVE_RTF_PACKAGE, "Term");
+
   private final ScriptingContainer scriptingContainer = initScriptingContainer();
+  private final Array<Vocab> vocabs = vocabsMap().toArray().map(this::toVocab).sorted();
+  private final Set<String> ambiguousTerms = findAmbiguousTerms(vocabs);
 
   void generate() {
-    Array<Vocab> vocabs = vocabsMap().toArray().map(this::toVocab);
-    vocabs.forEach(System.out::println);
+    // TODO: add enum methods, fields, bodies
+
+    TypeSpec.Builder vocabsBuilder = vocabs.foldLeft(
+      TypeSpec.enumBuilder(VOCAB_CLASS_NAME),
+      (b, v) -> v.addVocabEnumConstant(b)
+    );
+    TypeSpec vocabsSpec = vocabsBuilder.build();
+    JavaFile.Builder vocabsFileBuilder = JavaFile.builder(CURSIVE_RTF_PACKAGE, vocabsSpec);
+
+    TypeSpec.Builder termsBuilder = vocabs.foldLeft(
+      TypeSpec.enumBuilder(TERM_CLASS_NAME),
+      (b, v) ->
+        v.getTerms().foldLeft(b, (b1, t) ->
+          b1.addEnumConstant(getTermConstName(v, t))
+        )
+    );
+    TypeSpec termsSpec = termsBuilder.build();
+    JavaFile.Builder termsFileBuilder = JavaFile.builder(CURSIVE_RTF_PACKAGE, termsSpec);
+
+    List<JavaFile.Builder> fileBuilders = List.of(vocabsFileBuilder, termsFileBuilder);
+    List<JavaFile> files = fileBuilders.map(JavaFile.Builder::build);
+    files.forEach(f -> {
+      try {
+        f.writeTo(System.out);
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    });
+  }
+
+  // ------------------------------------------------------------
+  // Code generation helpers
+
+  private String getTermConstName(Vocab vocabulary, String term) {
+    String vocabConst = vocabulary.getConstName();
+    String termConstBase = Vocab.toConstName(term);
+    return ambiguousTerms.contains(term) ? String.format("%s_%s", termConstBase, vocabConst) : termConstBase;
   }
 
   // ------------------------------------------------------------
@@ -32,7 +76,8 @@ class Generators {
     String fqConstName = "RDF::Vocab::" + params.get("class_name").map(Object::toString).getOrElse(id.toUpperCase());
     String prefix = scriptingContainer.runScriptlet(fqConstName + ".__prefix__").toString();
     URI iri = URI.create(scriptingContainer.runScriptlet(fqConstName + ".to_iri").toString());
-    Array<String> terms = Array.ofAll((RubyArray) scriptingContainer.runScriptlet(fqConstName + ".send(:props).keys")).map(Object::toString);
+    Array<String> terms = Array.ofAll((Iterable<Object>) scriptingContainer.runScriptlet(fqConstName + ".send(:props).keys")).map(Object::toString)
+      .removeAll(StringUtils::isBlank);
 
     return new Vocab(prefix, iri, terms);
   }
@@ -44,6 +89,18 @@ class Generators {
 
   // ------------------------------------------------------------
   // Class methods
+
+  private static Set<String> findAmbiguousTerms(Array<Vocab> vocabs) {
+    return vocabs.foldLeft(HashMap.<String, Integer>empty(),
+      (m, v) ->
+        v.getTerms().foldLeft(m,
+          (m1, t) ->
+            m1.put(t, m1.get(t).map(c -> c + 1).getOrElse(1))
+        )
+    ).toSet()
+      .filter(t -> t._2 > 1)
+      .map(Tuple2::_1);
+  }
 
   private static LinkedHashMap<String, ?> toJava(RubyHash h) {
     return LinkedHashMap.ofAll(h).map((k, v) -> {
--- cursive-inv-codegen/src/main/java/org/cdlib/cursive/Vocab.java
+++ cursive-inv-codegen/src/main/java/org/cdlib/cursive/Vocab.java
@@ -1,14 +1,16 @@
 package org.cdlib.cursive;
 
+import com.squareup.javapoet.TypeSpec;
 import io.vavr.collection.Array;
 
 import java.net.URI;
 import java.util.Objects;
 
-class Vocab {
+class Vocab implements Comparable<Vocab> {
   private final String prefix;
   private final URI uri;
   private final Array<String> terms;
+  private final String constName;
 
   Vocab(String prefix, URI uri, Array<String> terms) {
     Objects.requireNonNull(prefix);
@@ -16,13 +18,44 @@ class Vocab {
     Objects.requireNonNull(terms);
 
     this.prefix = prefix;
+    this.constName = toConstName(prefix);
     this.uri = uri;
     this.terms = terms;
   }
 
+  static String toConstName(String rValue) {
+    return rValue
+      .replaceAll("/+$", "")
+      .replaceAll("([a-z])([A-Z])", "$1_$2")
+      .toUpperCase()
+      .replaceAll("^([^_A-Z])", "_$1")
+      .replaceAll("[^_$A-Z0-9]", "_");
+  }
+
+  public String getPrefix() {
+    return prefix;
+  }
+
+  public URI getUri() {
+    return uri;
+  }
+
+  public Array<String> getTerms() {
+    return terms;
+  }
+
+  public String getConstName() {
+    return constName;
+  }
+
+  TypeSpec.Builder addVocabEnumConstant(TypeSpec.Builder vocabsBuilder) {
+    return vocabsBuilder.addEnumConstant(constName);
+  }
+
   @Override
   public String toString() {
-    return prefix + ": " + uri + " [" + terms.mkString(", ") + "]";
+    String termList = terms.map(t -> String.format("%s (%s)", t, toConstName(t))).mkString(", ");
+    return String.format("%s (%s): %s [%s]", prefix, constName, uri, termList);
   }
 
   @Override
@@ -47,5 +80,20 @@ class Vocab {
     int result = prefix.hashCode();
     result = 31 * result + uri.hashCode();
     return result;
+  }
+
+  @Override
+  public int compareTo(Vocab o) {
+    Objects.requireNonNull(o);
+    if (o == this) {
+      return 0;
+    }
+
+    int order = prefix.compareTo(o.prefix);
+    if (order == 0) {
+      throw new IllegalStateException(String.format("Prefix \"%s\" shared by two vocabularies: <%s>, <%s>", prefix, this, o));
+    }
+
+    return order;
   }
 }
